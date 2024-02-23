30446fec258d8af490946d5e067378b0d7457d0e
Jan Forman <48495095+janforman@users.noreply.github.com>
2024-02-23 10:51:12 +0100

    Update gettrain.php
    New interface

--- gettrain.php
+++ gettrain.php
@@ -1,27 +1,43 @@
-<?php
 require('./config.php');
 $soap = '<?xml version="1.0" encoding="utf-8"?>
-<soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
-  <soap:Body>
-    <GetTrainPosition xmlns="http://provoz.szdc.cz/grappws/">
-      <_request>
-        <MessageHeader>
-          <MessageNumber>1</MessageNumber>
-          <MessageDateTime>' . date(DateTime::RFC3339, time()). '</MessageDateTime>
-        </MessageHeader>
-        <User>
-          <Login>' . $uid . '</Login>
-          <Password>' . $password . '</Password>
-        </User>
-        <LastUpdate>' . date(DateTime::RFC3339, time()- 180). '</LastUpdate>
-      </_request>
-    </GetTrainPosition>
-  </soap:Body>
+<soap:Envelope xmlns:soap="http://www.w3.org/2003/05/soap-envelope" xmlns:pov="https://wcf.grapp.spravazeleznic.cz/POVLData">
+  <soap:Header xmlns:wsa="http://www.w3.org/2005/08/addressing">
+  <wsa:To>' . $url . '</wsa:To>
+  <wsa:Action>https://wcf.grapp.spravazeleznic.cz/POVLData/IPOVLData/Train</wsa:Action>
+  </soap:Header>
+<soap:Body>
+      <pov:Train>
+        <pov:request>
+            <pov:MessageHeader>
+               <pov:MessageReference>
+                  <pov:MessageType>8012</pov:MessageType>
+                  <pov:MessageTypeVersion>2.4</pov:MessageTypeVersion>
+                  <pov:MessageIdentifier>1</pov:MessageIdentifier>
+                  <pov:MessageDateTime>' . date(DateTime::RFC3339, time()). '</pov:MessageDateTime>
+               </pov:MessageReference>
+            </pov:MessageHeader>
+            <pov:User>
+               <pov:Login>' . $uid . '</pov:Login>
+               <pov:Password>' . $password . '</pov:Password>
+            </pov:User>
+            <pov:LastUpdate>' . date(DateTime::RFC3339, time()- 180). '</pov:LastUpdate>
+         </pov:request>
+      </pov:Train>
+   </soap:Body>
 </soap:Envelope>
 ';
-$options = array('http' => array('method' => 'POST', 'header' => "Content-type: text/xml\r\n" . 'Content-Length: ' . strlen($soap). "\r\n", 'content' => $soap));
-$context = stream_context_create($options);
-$result = file_get_contents($url, false, $context);
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL,$url);
+curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/soap+xml'));
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS,$soap);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+$result = curl_exec($ch);
+curl_close($ch);
+
 $result = explode('</MessageHeader>', $result);
 $result = explode('<Status>OK</Status>', $result[1]);
 $xml = simplexml_load_string('<document>' . $result[0] . '</document>');
